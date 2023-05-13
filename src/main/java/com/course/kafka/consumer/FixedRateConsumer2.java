c2c16d93bdfeedcba08c78c158d002fa0f72ada7
babacamara91 <babacamara91@gmail.com>
2023-05-13 11:42:52 +0200

    consommer les messages en temps réel avec en mode earliest

---
+++ src/main/java/com/course/kafka/consumer/FixedRateConsumer2.java
@@ -0,0 +1,16 @@
+package com.course.kafka.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRateConsumer2 {
+    private static final Logger LOG = LoggerFactory.getLogger(FixedRateConsumer2.class);
+
+    @KafkaListener(topics = "t-fixedrate-2")
+    public void consume(String message){
+        LOG.info("Consuming : {}", message);
+    }
+}
